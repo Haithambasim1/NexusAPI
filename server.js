a6d6227dd7d301c45bdf4793d5a2009751fbf7cd
hp <haitham@gmail.com>
2025-03-19 10:03:52 +0200

    fix the .env

--- server.js
+++ server.js
@@ -15,7 +15,11 @@ const PORT = process.env.PORT || 3000;
 
 // Apply middleware
 app.use(helmet());
-app.use(cors());
+app.use(
+  cors({
+    origin: '*',
+  })
+);
 app.use(express.json());
 
 // Mount API routes
